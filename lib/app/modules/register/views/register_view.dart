8c6af38364db301744674091a2395e9f3b3fcd2c
Gagayogaa <yoghakurniawan35@gmail.com>
2024-03-07 08:22:08 +0700

    first commit

---
+++ lib/app/modules/register/views/register_view.dart
@@ -0,0 +1,200 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../components/customTextField.dart';
+import '../../../routes/app_pages.dart';
+import '../controllers/register_controller.dart';
+
+class RegisterView extends GetView<RegisterController> {
+  const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    const Color primary = Color(0xFFFF0000);
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+              child: Form(
+                key: controller.formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 15),
+                      child: Image.asset(
+                        'lib/assets/image/logo_black.png',
+                        fit: BoxFit.cover,
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                    AutoSizeText(
+                      'Sign up to make your personal account',
+                      maxLines: 2,
+                      maxFontSize: 25,
+                      minFontSize: 20,
+                      style: GoogleFonts.inter(
+                          color: Colors.black,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w700),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: CustomTextField(
+                        labelText: "Username",
+                        controller: controller.usernameController,
+                        hinText: "Ryan",
+                        obsureText: false,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Pleasse input username';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child:  CustomTextField(
+                        labelText: "Email",
+                        controller: controller.emailController,
+                        hinText: "ryan@smk.belajar.id",
+                        obsureText: false,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Pleasse input email address';
+                          } else if (!EmailValidator.validate(value)) {
+                            return 'Email address tidak sesuai';
+                          } else if (!value.endsWith('@smk.belajar.id')) {
+                            return 'Email harus @smk.belajar.id';
+                          }
+
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Obx(
+                            () => CustomTextField(
+                              labelText: "Password",
+                              controller: controller.passwordController,
+                              hinText: "password",
+                              obsureText: controller.isPasswordHidden.value,
+                              suffixIcon: InkWell(
+                                child: Icon(
+                                  controller.isPasswordHidden.value
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  size: 20,
+                                  color: primary,
+                                ),
+                                onTap: () {
+                                  controller.isPasswordHidden.value =
+                                      !controller.isPasswordHidden.value;
+                                },
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Pleasse input password';
+                                }
+
+                                return null;
+                              },
+                            ),
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.only(top: 50, bottom: 30),
+                            child: SizedBox(
+                              width: width,
+                              height: 50,
+                              child: ElevatedButton(
+                                onPressed: () => controller.registerPost(),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: primary,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                ),
+                                child: Obx(
+                                  () => controller.loadinglogin.value
+                                      ? const CircularProgressIndicator(
+                                          color: Colors.white,
+                                        )
+                                      : Text(
+                                          "Register",
+                                          style: TextStyle(
+                                              fontSize: 23,
+                                              fontWeight: FontWeight.w600,
+                                              color: Colors.white),
+                                        ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
+                    textToRegister(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textToRegister() {
+    return SizedBox(
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FittedBox(
+            child: Text(
+              'Already have an account??',
+              style: GoogleFonts.inter(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Get.offAllNamed(Routes.LOGIN);
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.transparent,
+            ),
+            child: FittedBox(
+              child: Text('Login',
+                  style: GoogleFonts.inter(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFF4CB3E0),
+                  )),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/customTextField.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController  controller;
+  final String hinText;
+  final String labelText;
+  final bool obsureText;
+  final Widget? suffixIcon;
+  final FormFieldValidator<String>? validator;
+
+  CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hinText,
+    required this.labelText,
+    required this.obsureText,
+    this.suffixIcon,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const Color primary = Color(0xFFFF0000);
+    const Color focusColor = Color(0xFF44EAC2);
+    const Color labelColor = Color(0xFF8A8586);
+
+    return TextFormField(
+      obscureText: obsureText,
+      controller: controller,
+      style: GoogleFonts.inter(
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      decoration: InputDecoration(
+        labelText: labelText,
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        labelStyle: TextStyle(
+          fontSize: 18,
+          color: labelColor,
+          fontWeight: FontWeight.w600,
+        ),
+        suffix: suffixIcon,
+        border: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: primary.withOpacity(0.90),
+          ),
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: primary.withOpacity(0.90),
+          ),
+        ),
+        disabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: primary.withOpacity(0.90),
+          ),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: focusColor.withOpacity(0.90),
+          ),
+        ),
+        contentPadding: EdgeInsets.symmetric(vertical: 10.0),
+        hintText: hinText,
+        hintStyle: GoogleFonts.inter(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          color: Colors.grey,
+        ),
+      ),
+      validator: validator,
+    );
+  }
+}
